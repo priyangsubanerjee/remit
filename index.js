497a8fc774a3e4106c6ce8c7f5d7364c9bf2beed
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-02-18 21:27:28 +0530

    prefilight

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 app.use(bodyParser.json());
