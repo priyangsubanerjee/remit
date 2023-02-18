7843a1a42771c9610efaa0aee64928c8cb8f7d5e
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-02-18 21:10:37 +0530

    origin

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const clientGraph = new GraphQLClient(process.env.GRAPH_API, {
   },
 });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 app.use(bodyParser.json());
 app.use("/", express.static(path.join(__dirname, "public")));
 
