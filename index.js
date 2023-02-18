2a102a8c1e1296262d7ba35b27e2eb805b95ccb6
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-02-18 22:48:50 +0530

    user created

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const port = 3000 || process.env.PORT;
 const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { gql, GraphQLClient } = require("graphql-request");
+const dotenv = require("dotenv");
+const nodemailer = require("nodemailer");
+
+dotenv.config();
+
+const graphClient = new GraphQLClient(process.env.GRAPH_API, {
+  headers: {
+    authorization: "Bearer " + process.env.GRAPH_TOKEN,
+  },
+});
 
 app.use(
   cors({
@@ -13,16 +24,57 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-
 app.use(express.static(path.join(__dirname, "public")));
 
-app.post("/create", (req, res) => {
+app.post("/create", async (req, res) => {
   const { name, email, password } = req.body;
-  res.json({
-    name,
-    email,
-    password,
-  });
+
+  const query = gql`
+    mutation MyMutation {
+      createClient(data: { name: "${name}", email: "${email}", password: "${password}" }) {
+        id
+      }
+    }
+  `;
+
+  try {
+    const { createClient } = await graphClient.request(query);
+    if (createClient.id) {
+      var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
+      var options = {
+        from: `Remit API <${process.env.EMAIL}>`,
+        to: email,
+        subject: "Welcome to Remit API",
+        html: `<h1>Welcome to Remit API</h1>
+          <p>Hi ${name},</p>    
+          <p>Thank you for registering with Remit API. We are glad to have you on board.</p>
+          <p>Your api token is: ${createClient.id}</p>
+          <p>Regards,</p>
+          <p>Remit API</p>`,
+      };
+      transporter.sendMail(options, function (err, info) {
+        if (err) {
+          console.log(err);
+          res.status(400).send({ message: "Email not sent !" });
+        } else {
+          res
+            .status(200)
+            .send({ message: "User created & email sent successfully" });
+        }
+      });
+    } else {
+      res.status(400).send({ message: "User not created" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ message: "User not created" });
+  }
 });
 
 app.listen(port, () => {
