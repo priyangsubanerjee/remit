9cd57873c1032ce930dafce0221e98a457ce6c35
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-02-18 20:47:39 +0530

    css loader

--- public/script.js
+++ public/script.js
@@ -2,9 +2,11 @@ const gmailForm = document.querySelector(".gmailForm");
 const gmailName = document.querySelector(".gmailName");
 const gmailEmail = document.querySelector(".gmailEmail");
 const gmailAppPassword = document.querySelector(".gmailAppPassword");
+const loader = document.querySelector(".loader");
 
 gmailForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  loader.classList.add("active");
   axios
     .post("/create", {
       name: gmailName.value,
@@ -13,8 +15,12 @@ gmailForm.addEventListener("submit", (e) => {
     })
     .then((res) => {
       console.log(res);
+      loader.classList.remove("active");
+      alert("Api token sent to your email !");
     })
     .catch((err) => {
       console.log(err);
+      loader.classList.remove("active");
+      alert("Error occured !");
     });
 });
